b1cecf5fe20c48b3f98085b9164cff1162fa04dc
Joydip Mondal <joydip.mondal@gmail.com>
2025-12-02 14:19:30 +0530

    more on data binding and if, else, switch, case, default, for etc.

--- codes/day-2/binding-app/src/app/app.ts
+++ codes/day-2/binding-app/src/app/app.ts
@@ -1,10 +1,11 @@
+//import { NgFor, NgIf } from '@angular/common';
 import { Component } from '@angular/core';
-
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.html',
-  styleUrl: './app.css'
+  styleUrl: './app.css',
+  //imports:[NgFor,NgIf]
 })
 export class App {
   name = 'Joydip'
@@ -14,6 +15,21 @@ export class App {
     id: 1,
     name: 'joy'
   }
+
+  numbers = [1, 2, 3, 4, 5]
+  choice = 0
+  onChoiceEntered(val: number) {
+    this.choice = val
+  }
+  // constructor() {
+
+  //   for (let x of this.numbers) {
+  //     console.log(x);
+  //   }
+  //   for (let i in this.numbers) {
+  //     console.log(this.numbers[i]);
+  //   }
+  // }
 
   // onInput(e: Event) {
   //   const element: HTMLInputElement = e.target as HTMLInputElement;
