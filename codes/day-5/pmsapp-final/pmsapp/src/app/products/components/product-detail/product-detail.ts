7ede2a3af1d633928c914d4256a4b5b650c505e7
Joydip Mondal <joydip.mondal@gmail.com>
2025-12-05 12:33:24 +0530

    ProductDetail code completed

--- codes/day-5/pmsapp-final/pmsapp/src/app/products/components/product-detail/product-detail.ts
+++ codes/day-5/pmsapp-final/pmsapp/src/app/products/components/product-detail/product-detail.ts
@@ -1,4 +1,10 @@
-import { Component } from '@angular/core';
+import { Component, Inject, OnInit, signal } from '@angular/core';
+import { ActivatedRoute, ActivatedRouteSnapshot, Params } from '@angular/router';
+import { PRODUCT_SERVICE_TOKEN } from '../../../config/constants';
+import { IProductService } from '../../services/product-service.contract';
+import { Subscription } from 'rxjs';
+import { ApiResponse } from '../../../models/api-response';
+import { Product } from '../../../models/product';
 
 @Component({
   selector: 'app-product-detail',
@@ -6,6 +12,47 @@ import { Component } from '@angular/core';
   templateUrl: './product-detail.html',
   styleUrl: './product-detail.css',
 })
-export class ProductDetail {
+export class ProductDetail implements OnInit {
 
+  private prodSubscription?: Subscription;
+  product = signal<Product | undefined>(undefined)
+  isFetchComplete = signal(false)
+  errorInfo = signal('')
+
+  constructor(
+    private currentRoute: ActivatedRoute,
+    @Inject(PRODUCT_SERVICE_TOKEN) private prodSvc: IProductService
+  ) {
+  }
+
+  ngOnInit(): void {
+    //this.currentRoute.params;
+    const routeSnapshot: ActivatedRouteSnapshot = this.currentRoute.snapshot;
+
+    const currentRouteParams: Params = routeSnapshot.params;
+    const productId = Number(currentRouteParams['id'])
+
+    this.prodSubscription =
+      this
+        .prodSvc
+        .getProduct(productId)
+        .subscribe({
+          next: (response: ApiResponse<Product>) => {
+            if (response.data !== null) {
+              this.product.set(response.data)
+              this.isFetchComplete.set(true)
+              this.errorInfo.set('')
+            } else {
+              this.product.set(undefined)
+              this.isFetchComplete.set(true)
+              this.errorInfo.set(response.message)
+            }
+          },
+          error: (err) => { 
+            this.product.set(undefined)
+            this.isFetchComplete.set(true)
+            this.errorInfo.set(err.message)
+          }
+        })
+  }
 }
