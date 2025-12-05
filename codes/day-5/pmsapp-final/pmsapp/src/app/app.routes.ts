6cd6ab25d40cc29e3e778ee8a38cb2c3cd54f9ea
Joydip Mondal <joydip.mondal@gmail.com>
2025-12-05 10:43:42 +0530

    auth routes updated

--- codes/day-5/pmsapp-final/pmsapp/src/app/app.routes.ts
+++ codes/day-5/pmsapp-final/pmsapp/src/app/app.routes.ts
@@ -3,8 +3,10 @@ import { ProductContainer } from './products/components/product-container/produc
 import { ProductDetail } from './products/components/product-detail/product-detail';
 import { AddProduct } from './products/components/add-product/add-product';
 import { EditProduct } from './products/components/edit-product/edit-product';
+import { Registration } from './auth/components/registration/registration';
+import { Login } from './auth/components/login/login';
 
-export const productRoutes: Routes = [
+export const appRoutes: Routes = [
     {
         path: 'products',
         children: [
@@ -19,6 +21,17 @@ export const productRoutes: Routes = [
             },
             {
                 path: 'edit/:id', component: EditProduct
+            }
+        ]
+    },
+    {
+        path: 'auth',
+        children: [
+            {
+                path: 'register', component: Registration
+            },
+            {
+                path: 'login', component: Login
             }
         ]
     }
