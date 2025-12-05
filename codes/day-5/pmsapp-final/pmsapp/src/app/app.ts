751e15fbe1d8664baa64f0188416d2bdf45e6ffd
Joydip Mondal <joydip.mondal@gmail.com>
2025-12-05 11:46:59 +0530

    dashboard created with style

--- codes/day-5/pmsapp-final/pmsapp/src/app/app.ts
+++ codes/day-5/pmsapp-final/pmsapp/src/app/app.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { DashBoard } from './common/components/dash-board/dash-board';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, DashBoard],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
